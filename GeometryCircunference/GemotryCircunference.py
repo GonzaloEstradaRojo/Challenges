6f789f7e813ae48fae4af5c50feb212e2acd67d5
Gonzalo Estrada Rojo <48635918+GonzaloEstradaRojo@users.noreply.github.com>
2023-01-23 01:54:58 +0100

    Graphics updated. Not optimized for large cols.

--- GeometryCircunference/GemotryCircunference.py
+++ GeometryCircunference/GemotryCircunference.py
@@ -5,8 +5,8 @@ pygame.init()
 pygame.display.set_caption("Geometry")
 clock = pygame.time.Clock()
 
-fps_limit = 60
-cols, rows = 5, 5
+fps_limit = 120
+cols, rows = 5,5
 width, height = 800, 800
 screen = pygame.display.set_mode((width,int(height)))
 screen.fill((0,0,0))
@@ -26,7 +26,6 @@ def GameLoop():
     run, pause = True, False
     angleRotation, last_pause = 0, 0
     rand = [random.uniform(0,1) < 0.75 for _ in range(cols*rows)]
-    press = None
     try:
         DrawPiezas()
         while run:
@@ -34,11 +33,7 @@ def GameLoop():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_a:
-                         press = True 
             pause = (angleRotation != 0 and angleRotation % 90 == 0)
-            # if press:
             if not pause:
                 for i,pieza in enumerate(piezas):
                     pieza.RotarPieza(rand[i])
@@ -49,7 +44,6 @@ def GameLoop():
                     pause = False
                     angleRotation = 0
                     rand = [random.uniform(0,1) < 0.75 for _ in range(cols*rows)]
-                    # press = False
     except Exception as error:
         raise(error)
 
--- GeometryCircunference/Pieza.py
+++ GeometryCircunference/Pieza.py
@@ -5,7 +5,7 @@ class Pieza():
         self._pos = pos
         self._diameter = diameter
         self._radius = self._diameter/2
-        self._border = 5
+        self._border = 0
         self._cirThick = 10
         self._screen = screen
         self._background = pygame.Surface((self._diameter - self._border, self._diameter - self._border), pygame.SRCALPHA) 
@@ -26,34 +26,11 @@ class Pieza():
 
     def DrawCircle(self):
         if(self._seed == 0):
-            # pygame.draw.arc(self._surface, (255,0,0), 
-            #                 (-self._radius + self._cirThick/2,-self._radius + self._cirThick/2,self._diameter ,self._diameter ),
-            #                 math.pi*3/2,0,self._cirThick)
-
-            # pygame.draw.arc(self._surface, (0,255,0),
-            #                 (self._radius - self._cirThick/2, self._radius - self._cirThick/2,self._diameter ,self._diameter ),
-            #                 math.pi/2,math.pi,self._cirThick)
-
-            pygame.draw.circle(self._surface, (0,255,0),(self._cirThick/2, self._cirThick/2), self._radius, self._cirThick)
             pygame.draw.circle(self._surface, (255,0,0),(self._diameter - self._cirThick/2, self._diameter - self._cirThick/2), self._radius, self._cirThick)
-
-
-            # pygame.draw.arc(self._surface, (0,255,0),
-            #                 (self._radius - self._cirThick/2, self._radius - self._cirThick/2,self._diameter ,self._diameter ),
-            #                 math.pi/2,math.pi,self._cirThick)
-
+            pygame.draw.circle(self._surface, (255,0,0),(self._cirThick/2, self._cirThick/2), self._radius, self._cirThick)
         elif(self._seed == 1):
-            # pygame.draw.arc(self._surface, (0,0,255),
-            #                 (-self._radius + self._cirThick/2,self._radius - self._cirThick/2,self._diameter,self._diameter),
-            #                 0,math.pi/2,self._cirThick)
-
-            # pygame.draw.arc(self._surface, (0,0,0),
-            #                 (self._radius - self._cirThick/2,-self._radius,self._diameter ,self._diameter),
-            #                 math.pi,math.pi*3/2,self._cirThick)
-            pygame.draw.circle(self._surface, (0,0,255),(self._cirThick/2, self._diameter - self._cirThick/2), self._radius, self._cirThick)
-            pygame.draw.circle(self._surface, (0,0,0),(self._diameter - self._cirThick/2 , self._cirThick/2), self._radius, self._cirThick)
-
-
+            pygame.draw.circle(self._surface, (255,0,0),(self._cirThick/2, self._diameter - self._cirThick/2), self._radius, self._cirThick)
+            pygame.draw.circle(self._surface, (255,0,0),(self._diameter - self._cirThick/2 , self._cirThick/2), self._radius, self._cirThick)
         self._screen.blit(self._surface , self._rect)  
 
     def RotarPieza(self,dir):
@@ -65,7 +42,6 @@ class Pieza():
         new_rect = new_image.get_rect()  
         new_rect.center = old_center  
         print(self._angle)
-
         self._screen.blit(self._background , self._rect)  
         self._screen.blit(new_image , new_rect)  
 
